b8a21ef71c92d314ad802f5ff738781f741b0c63
Isaiah <dothackdasen@gmail.com>
2020-09-28 22:27:58 -0700

    Built out game

--- AbilityScoreCalculator/Program.cs
+++ AbilityScoreCalculator/Program.cs
@@ -6,7 +6,61 @@ namespace AbilityScoreCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            while (true)
+            {
+                calculator.Roll = ReadInt(calculator.Roll, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+                calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Calculated ability score: " + calculator.Score);
+                Console.WriteLine("Press Q to quite, any other key to continue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+            }
+        }
+
+        private static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+            if (double.TryParse(line, out double value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine(" using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+
+        /// <summary>
+        /// Writes a prompt and reads an int value from the console.
+        /// </summary>
+        /// <param name="lastUsedValue">The default value.</param>
+        /// <param name="prompt">Prompt to print to the console.</param>
+        /// <returns>The int value read, or the default value if unable to parse</returns>
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            /// Write the prompy followed by [default value]:
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            /// Read the line from the input and try to parse it
+            string line = Console.ReadLine();
+            /// If it can be parsed, write " using value" + value to console
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+                /// Otherwise write" using default value" + lastUsedValue to the console
+            }
+            else
+            {
+                Console.WriteLine(" using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
         }
     }
 
@@ -24,7 +78,7 @@ namespace AbilityScoreCalculator
             double divided = Roll / DivideBy;
 
             // Add to the result
-            int added = AddAmount += divided;
+            int added = AddAmount += (int)divided;
 
             // Make sure it's greater than the minimum
             if (added < Minimum)
